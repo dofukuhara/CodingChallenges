9b46c686784befa3041f017cafa6a675499dd99b
Douglas Fukuhara <dofukuhara@gmail.com>
2024-02-06 10:10:20 -0800

    Two Sum coding challenge
    ************************ Two Sum ************************
    * Given an array of integers nums and an integer target,*
    * return indices of the two numbers such that they add  *
    * up to target.                                         *
    * You may assume that each input would have exactly one *
    * solution, and you may not use the same element twice. *
    * You can return the answer in any order.               *
    *                                                       *
    * Example 1:                                            *
    *    | Input: nums = [2,7,11,15], target = 9            *
    *    | Output: [0,1]                                    *
    *    | Explanation:                                     *
    *      Because nums[0] + nums[1] == 9, we return [0, 1].*
    * Example 2:                                            *
    *    | Input: nums = [3,2,4], target = 6                *
    *    | Output: [1,2]                                    *
    * Example 3:                                            *
    *    | Input: nums = [3,3], target = 6                  *
    *    | Output: [0,1]                                    *
    *********************************************************

---
+++ src/main/kotlin/twosum/Tester.kt
@@ -0,0 +1,11 @@
+package twosum
+
+fun main(args: Array<String>) {
+    val solution = TwoSum()
+
+    solution.twoSum(intArrayOf(2,7,11,15), 9)
+    solution.twoSum(intArrayOf(3,2,4), 6)
+    solution.twoSum(intArrayOf(3,3), 6)
+    solution.twoSum(intArrayOf(3,2), 6)
+
+}

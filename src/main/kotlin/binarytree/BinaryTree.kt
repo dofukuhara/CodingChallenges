d9a0ebf20cb3587c7fade24bded1eee3ec0a35a9
Douglas Fukuhara <dofukuhara@gmail.com>
2024-02-13 23:44:38 -0800

    Add Binary coding challenge
    ********************* Add Binary **********************
    * Given two binary strings a and b, return their sum  *
    * as a binary string.                                 *
    *                                                     *
    * Example 1:                                          *
    *    | Input: a = "11", b = "1"                       *
    *    | Output: "100"                                  *
    *                                                     *
    * Example 2:                                          *
    *    | Input: a = "1010", b = "1011"                  *
    *    | Output: "10101"                                *
    *                                                     *
    * Constraints:                                        *
    *    | 1 <= a.length, b.length <= 10^4                *
    *    | a and b consist only of '0' or '1' characters. *
    *    | Each string does not contain leading zeros     *
    *      except for the zero itself.                    *
    *******************************************************

--- src/main/kotlin/binarytree/BinaryTree.kt
+++ src/main/kotlin/binarytree/BinaryTree.kt
@@ -44,6 +44,15 @@ class BinaryTree {
             print(printTree.toString())
         }
     }
+
+    fun maxDepth(root: TreeNode?): Int {
+        if (root == null) return 0
+
+        val leftDepth = maxDepth(root.left)
+        val rightDepth = maxDepth(root.right)
+
+        return maxOf(leftDepth, rightDepth) + 1
+    }
 }
 
 sealed class BfsNode {
